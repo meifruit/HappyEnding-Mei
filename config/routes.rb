09c4932e72f9b09153bb923364d353b16e2debde
exec zsh <wind10cloud@gmail.com>
2023-05-15 16:26:05 +0900

    created controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  # root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "services#home"
+  resources :services, only: [:index, :show, :new, :create]
+  resources :bookings, except: :destroy
 end
