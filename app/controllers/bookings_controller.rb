104431d1520cc30081c2c6a1348624e439eb2c56
exec zsh <wind10cloud@gmail.com>
2023-05-15 18:20:12 +0900

    changed routes and bookingscontroller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,28 @@
 class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+    @service = Service.find(params[:service_id])
+  end
+
+  def create
+    @booking = Booking.new(params_booking)
+    @user = current_user
+    @service = Sercvice.new(params[:service_id])
+    @booking.user = @user
+    @booking.service = @service
+    if @booking.save
+      redirect_to service_path(Service.find(params[:service_id]))
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def params_booking
+    params.require(:booking).permit(:user_id, :service_id, :start_date, :end_date)
+  end
+
 end
