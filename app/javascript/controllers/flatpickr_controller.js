d15b028b918ae5e1718b18823adc5e9f448fe5a7
exec zsh <wind10cloud@gmail.com>
2023-05-16 16:43:59 +0900

    changes new views

---
+++ app/javascript/controllers/flatpickr_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
+
+
+export default class extends Controller {
+  static targets = ["startTime", "endTime"]
+  connect() {
+    flatpickr(this.startTimeTarget, {
+      enableTime: true,
+      plugins: [new rangePlugin({ input: "#end_time" })]
+    })
+    flatpickr(this.endTimeTarget, {})
+  }
+}
+
+// Connects to data-controller="flatpickr"
+// export default class extends Controller {
+  // connect() {
+  // }
+// }
